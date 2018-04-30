d8a8c1ae8391eaba04285bd07a7e2dc62c2d66d5
Andrzej Dybowski <dybowski@andrzejd.pl>
2018-04-30 12:35:26 +0200

    - add Test: RepositoryTest(getLastModifiedDate) - add Class: Repository:   *getLastModifiedDate

---
+++ src/main/java/com/dybowski_andrzej/Repository.java
@@ -0,0 +1,20 @@
+package com.dybowski_andrzej;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Repository {
+
+    private LocalDate lastModifiedDate;
+    private String name;
+
+    public Repository(String name, String dateTime, String pattern) {
+        this.name = name;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        this.lastModifiedDate = LocalDate.parse(dateTime, formatter);
+    }
+
+    public LocalDate getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+}
---
+++ src/test/java/com/dybowski_andrzej/RepositoryTest.java
@@ -0,0 +1,32 @@
+package com.dybowski_andrzej;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepositoryTest {
+
+    @Test
+    void getLastModifiedDate() {
+        String date = "2018-04-24T11:32:11Z";
+        date = date.replace("T"," ");
+        date = date.replace("Z"," ");
+
+        String name = "GitHub repository";
+
+        String pattern = "yyyy-MM-dd HH:mm:SS ";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        LocalDate correctDate = LocalDate.parse(date, formatter);
+
+        Repository repository = new Repository(name, date, pattern);
+
+        LocalDate dateRepository = repository.getLastModifiedDate();
+
+
+        Assertions.assertTrue(dateRepository.isEqual(correctDate));
+    }
+}
